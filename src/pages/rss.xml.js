a904cf764aca98b7aaa6345f5756526d1d164de1
Üllar Seerme <yates@protonmail.ch>
2023-02-10 08:51:18 +0200

    feat(#11): add full post content to RSS feeds

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
 
 export async function get(context) {
+  const postImportResult = import.meta.glob('./**/*.md', { eager: true }); 
+  const posts = Object.values(postImportResult);
   return rss({
     title: 'Üllar Seerme',
     description: 'Personal website',
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./**/*.md'),
-    ),
+    items: posts.map((post) => ({
+      link: post.url,
+      content: sanitizeHtml(post.compiledContent()),
+      ...post.frontmatter,
+    })),
     drafts: false,
     customData: `<language>en-us</language>`,
     stylesheet: '/pretty-feed-v3.xsl',
