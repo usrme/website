356eeb7c05c562be0c6b730e08ccfaff1fbc7a4e
Üllar Seerme <yates@protonmail.ch>
2023-04-27 12:26:40 +0300

    build(build.sh): add final improvements
    famous last words

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 MDBOOK_VERSION="v0.4.28"
+
+echo "Cleaning any new untracked files"
+git clean -df
 
 echo "Downloading mdBook ${MDBOOK_VERSION} and unpacking"
 curl -sL "https://github.com/rust-lang/mdBook/releases/download/${MDBOOK_VERSION}/mdbook-${MDBOOK_VERSION}-x86_64-unknown-linux-gnu.tar.gz" |
@@ -10,7 +15,8 @@ echo "Building snippets"
 ./mdbook build
 
 echo "Copying built snippets to 'public/'"
-cp -aR src/pages/snippets/* public/
+mkdir public/snippets
+cp -aR src/pages/snippets/* public/snippets/
 
 echo "Reverting changes made to favicon"
 git restore public/favicon.svg
