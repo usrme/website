0441b4d05da2cd3f62b8b50ebff3f3efa7edb03a
Üllar Seerme <yates@protonmail.ch>
2023-04-27 11:37:49 +0300

    build(build.sh): re-order steps

--- build.sh
+++ build.sh
@@ -2,15 +2,9 @@
 
 MDBOOK_VERSION="v0.4.28"
 
-echo "Removing potentially existing mdBook build directory"
-rm -rf src/pages/snippets
-
 echo "Downloading mdBook ${MDBOOK_VERSION} and unpacking"
 curl -sL "https://github.com/rust-lang/mdBook/releases/download/${MDBOOK_VERSION}/mdbook-${MDBOOK_VERSION}-x86_64-unknown-linux-gnu.tar.gz" |
   tar -xz
-
-echo "Building site"
-npm run build
 
 echo "Building snippets"
 ./mdbook build
@@ -20,3 +14,9 @@ cp -aR src/pages/snippets/* public/
 
 echo "Reverting changes made to favicon"
 git restore public/favicon.svg
+
+echo "Removing potentially existing mdBook build directory"
+rm -rf src/pages/snippets
+
+echo "Building site"
+npm run build
