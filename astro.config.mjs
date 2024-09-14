8a46fc64747dd6ea55517f6ad6d0e9750e688d9d
Üllar Seerme <yates@protonmail.ch>
2024-09-14 18:11:08 +0300

    refactor: remove Starlight
    it has been moved into its own repository for easier upkeep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config'
-import starlight from '@astrojs/starlight';
 
 export default defineConfig({
   site: 'https://usrme.xyz',
@@ -11,36 +10,4 @@ export default defineConfig({
       wrap: false,
     },
   },
-  integrations: [
-    starlight({
-      title: 'Back to home page',
-      customCss: [
-        './src/styles/starlight.css',
-      ],
-      sidebar: [
-        {
-          label: 'Start here',
-          items: [
-            { label: 'Ansible', link: '/snippets/ansible' },
-            { label: 'Docker/Podman', link: '/snippets/docker-podman' },
-            { label: 'Git', link: '/snippets/git' },
-            { label: 'Kubernetes', link: '/snippets/kubernetes' },
-            { label: 'PowerShell', link: '/snippets/powershell' },
-            { label: 'Shell', link: '/snippets/shell' },
-            { label: 'Vim', link: '/snippets/vim' },
-          ],
-        },
-      ],
-      head: [
-        {
-          tag: 'script',
-          attrs: {
-            src: '//gc.zgo.at/count.js',
-            'data-goatcounter': 'https://usrme.goatcounter.com/count',
-            defer: true,
-          },
-        },
-      ],
-    }),
-  ],
 })
