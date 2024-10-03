2dff994e990efd394b313bc7443bfca6573640bd
Üllar Seerme <yates@protonmail.ch>
2024-10-03 12:55:28 +0300

    fix: make sure line numbers end right after code

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,17 @@ export default defineConfig({
       theme: 'github-dark-dimmed',
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       wrap: false,
+      // https://github.com/shikijs/shiki/issues/3#issuecomment-2272168959
+      transformers: [
+        {
+          preprocess(code) {
+            if (code.endsWith('\n')) {
+              code = code.slice(0, -1)
+            }
+            return code
+          },
+        },
+      ],
     },
   },
 })
